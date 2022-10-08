c00bc36c43f011ff066414b0fbd52aeb8190a194
Marat <marato13@ya.ru>
2022-10-08 13:18:44 +0300

    add mobile header with js support

---
+++ source/js/header.js
@@ -0,0 +1,30 @@
+
+var navSite = document.querySelector ('.header__site-nav');
+var navToggle = document.querySelector ('.header__toggle');
+var navWrapper = document.querySelector ('.header__wrapper');
+
+navSite.classList.remove('header__site-nav--nojs');
+navToggle.classList.remove('header__toggle--nojs');
+navWrapper.classList.remove('header__wrapper--nojs');
+
+navSite.classList.add('header__site-nav--closed');
+navToggle.classList.add('header__toggle--closed');
+
+navToggle.addEventListener('click', function () {
+
+  if (navSite.classList.contains('header__site-nav--closed')) {
+    navSite.classList.remove('header__site-nav--closed');
+    navSite.classList.add('header__site-nav--open');
+  } else {
+    navSite.classList.add('header__site-nav--closed');
+    navSite.classList.remove('header__site-nav--open');
+  }
+
+  if (navToggle.classList.contains('header__toggle--closed')) {
+    navToggle.classList.remove('header__toggle--closed');
+    navToggle.classList.add('header__toggle--open');
+  } else {
+    navToggle.classList.add('header__toggle--closed');
+    navToggle.classList.remove('header__toggle--open');
+  }
+});
