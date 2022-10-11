5782b7a896c6c8a8ea2fff4acb04a76aaf7973ca
Marat <marato13@ya.ru>
2022-10-11 16:54:02 +0300

    All done checking

--- gulpfile.js
+++ gulpfile.js
@@ -90,7 +90,7 @@ const copy = (done) => {
   gulp.src([
     'source/fonts/**/*.{woff2,woff}',
     'source/*.ico',
-    'source/*.webmanifest',
+    'source/manifest.json'
   ], {
     base: 'source'
   })
