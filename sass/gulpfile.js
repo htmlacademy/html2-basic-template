51c70f839e8b5f601a8255035bca0f1b6794369f
firefoxic <firefoxic.dev@gmail.com>
2022-10-19 19:18:00 +0300

    Add w3c validator to automation

--- sass/gulpfile.js
+++ sass/gulpfile.js
@@ -11,6 +11,7 @@ import svgo from 'gulp-svgmin';
 import svgstore from 'gulp-svgstore';
 import {deleteAsync} from 'del';
 import browser from 'browser-sync';
+import { htmlValidator } from "gulp-w3c-html-validator";
 
 // Styles
 
@@ -33,6 +34,12 @@ export const styles = () => {
 const html = () => {
   return gulp.src('source/*.html')
     .pipe(gulp.dest('build'));
+}
+
+export const validateMarkup = () => {
+  return gulp.src('source/*.html')
+		.pipe(htmlValidator.analyzer())
+		.pipe(htmlValidator.reporter({ throwErrors: true }));
 }
 
 // Scripts
