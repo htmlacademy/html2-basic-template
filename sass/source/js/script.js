e7b0ce33281268c5b14463b62a063fad453ec34c
Boris Bakhtin <borisbakhtin@yandex.ru>
2023-02-23 00:39:03 +0300

    добавление swiper слайдера

--- sass/source/js/script.js
+++ sass/source/js/script.js
@@ -12,3 +12,17 @@ navToggle.addEventListener('click', function () {
     navMenu.classList.remove('menu-nav--opened');
   }
 });
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  });
