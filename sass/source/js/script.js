5530d3f98d36d7f8b443c54e713bf039685f8128
Boris Bakhtin <borisbakhtin@yandex.ru>
2023-02-22 07:50:34 +0300

    Добавление JS для навигации

--- sass/source/js/script.js
+++ sass/source/js/script.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+let navMenu = document.querySelector('.menu-nav');
+let navToggle = document.querySelector('.menu-nav__toggle');
+
+navMenu.classList.remove('menu-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMenu.classList.contains('menu-nav--closed')) {
+    navMenu.classList.remove('menu-nav--closed');
+    navMenu.classList.add('menu-nav--opened');
+  } else {
+    navMenu.classList.add('menu-nav--closed');
+    navMenu.classList.remove('menu-nav--opened');
+  }
+});
