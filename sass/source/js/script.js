c9ac95eea90a3b586f6391a7b3755dd3a85647f8
SergeiMart <konoplyankas@mail.ru>
2023-02-23 03:12:03 +0300

    Грейдирование

--- sass/source/js/script.js
+++ sass/source/js/script.js
@@ -1,1 +1,44 @@
-/* в этот файл добавляет скрипты*/
+let navMain = document.querySelector('.header__nav');
+let navToggle = document.querySelector('.header__button');
+
+navMain.classList.remove('header__nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('header__nav--closed')) {
+    navMain.classList.remove('header__nav--closed');
+    navMain.classList.add('header__nav--opened');
+  } else {
+    navMain.classList.add('header__nav--closed');
+    navMain.classList.remove('header__nav--opened');
+  }
+});
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  });
+
+  var map = L.map('map').setView([59.968302, 30.317429], 18);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/#map=18/59.96812/30.31687">OpenStreetMap</a>'
+}).addTo(map);
+
+var greenIcon = L.icon({
+  iconUrl: '/img/Map-Pin.png',
+
+
+  iconSize:     [38, 50],
+  iconAnchor:   [22, 44]
+});
+
+var marker = L.marker([59.968302, 30.317428], {icon: greenIcon}).addTo(map);
