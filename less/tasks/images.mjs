ceb0d9e414102309a2c22c8e67235f0fe27909db
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-04-23 22:53:58 +0300

    Remove 3x scale logic

--- less/tasks/images.mjs
+++ less/tasks/images.mjs
@@ -2,37 +2,33 @@ import gulp from "gulp";
 import sharpResponsive from "gulp-sharp-responsive";
 import cache from "gulp-cache";
 
-const RE_SIZERS = [(image) => image.width, (image) => Math.ceil((image.width * 2) / 3), (image) => Math.ceil(image.width / 3)];
-const RE_NAMERS = [{ suffix: "@3x" }, { suffix: "@2x" }];
+/**
+ * @typedef {({width: number}) => number} ResizeFn
+ * @type {ResizeFn[]} **/
+const RE_SIZERS = [({ width }) => width, ({ width }) => Math.ceil(width / 2)];
+const RENAME_OPTIONS = { suffix: "@2x" };
 const OPTIONS = createOptionsFormat();
 
-function optimizeImages() {
-	return gulp
-		.src('source/img/**/*.{png,jpg}')
-		.pipe(cache(sharpResponsive(OPTIONS)))
-		.pipe(gulp.dest('build/img'));
-}
-
 function createOptionsFormat() {
-	const formats = [];
+  const formats = [];
 
-	for (const format of [undefined, "avif", "webp"]) {
-		formats.push(
-			{
-				width: RE_SIZERS[0],
-				rename: RE_NAMERS[0],
-				format,
-			},
-			{
-				width: RE_SIZERS[1],
-				rename: RE_NAMERS[1],
-				format,
-			},
-			{ width: RE_SIZERS[2], format }
-		);
-	}
+  for (const format of [undefined, "avif", "webp"]) {
+    formats.push(
+      {
+        width: RE_SIZERS[0],
+        rename: RENAME_OPTIONS,
+        format,
+      },
+      { width: RE_SIZERS[1], format }
+    );
+  }
 
-	return {formats};
+  return { formats };
 }
 
-export { optimizeImages };
+export function optimizeImages() {
+  return gulp
+    .src("source/img/**/*.{png,jpg}")
+    .pipe(cache(sharpResponsive(OPTIONS)))
+    .pipe(gulp.dest("build/img"));
+}
