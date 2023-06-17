eabfa876e06eaaf27c90245ae506bce7c160eb5b
Miss_terry <Etein@yandex.ru>
2023-06-17 23:38:57 +0300

    Slider prepared for js

--- less/source/js/script.js
+++ less/source/js/script.js
@@ -1,1 +1,12 @@
-/* в этот файл добавляет скрипты*/
+const coffeeSwiper = new Swiper('.swiper', {
+  direction: "horizontal",
+  loop: !0,
+  navigation: {
+    nextEl: '.slider__button--next',
+    prevEl: '.slider__button--prev'
+  },
+  pagination: {
+    el: '.swiper__pagination',
+    clickable: true,
+  }
+});
