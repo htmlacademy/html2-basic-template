5b50f61a16119148b61c4643474cf3167bb6b8aa
IrinaLemeshkina <oleksinairina@gmail.com>
2023-06-25 20:58:22 +0300

    Правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+let navHeader = document.querySelector('.header-nav');
+let navToggle = document.querySelector('.header-nav__toggle');
+
+navHeader.classList.remove('header-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navHeader.classList.contains('header-nav--closed')) {
+    navHeader.classList.remove('header-nav--closed');
+    navHeader.classList.add('header-nav--opened');
+  } else {
+    navHeader.classList.add('header-nav--closed');
+    navHeader.classList.remove('header-nav--opened');
+  }
+});
