a753180c64a1a705e0b5c07c031ca9cda4d15e8e
ElviraFedchenko <ms.pulka93@gmail.com>
2023-06-28 21:06:20 +0300

    добавляет изображения и делает разметку

--- sass/source/js/script.js
+++ sass/source/js/script.js
@@ -1,1 +1,14 @@
 /* в этот файл добавляет скрипты*/
+document.querySelector('body').classList.remove('nojs')
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
