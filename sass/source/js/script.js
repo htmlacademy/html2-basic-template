4c272c899b340dc695cfe2c86b05fe1de863a0d9
ElviraFedchenko <ms.pulka93@gmail.com>
2023-07-02 01:34:50 +0300

    добавляет свайпер

--- sass/source/js/script.js
+++ sass/source/js/script.js
@@ -12,3 +12,22 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+
+});
