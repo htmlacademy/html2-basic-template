24c293e86717356f28d18735a329b25ecd7ef2ce
A.Voln <a.voln@list.ru>
2023-07-02 13:27:26 +0300

    markup finished

--- sass/source/js/script.js
+++ sass/source/js/script.js
@@ -1,1 +1,12 @@
-/* в этот файл добавляет скрипты*/
+document.querySelector('body').classList.remove('nojs');
+
+const mainNav = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navToggle.addEventListener('click', () => {
+  if (!mainNav.classList.contains('main-nav--opened')) {
+    mainNav.classList.add('main-nav--opened');
+  } else {
+    mainNav.classList.remove('main-nav--opened');
+  }
+});
