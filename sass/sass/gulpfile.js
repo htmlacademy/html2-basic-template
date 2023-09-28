a5001ede893de87ac14f151e4f5a5116af8e5498
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2023-09-28 13:54:32 +0300

    Обновляет сборку

---
+++ sass/sass/gulpfile.js
@@ -0,0 +1,157 @@
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import * as dartSass from "sass";
+import gulpSass from "gulp-sass";
+import postcss from 'gulp-postcss';
+import postUrl from 'postcss-url';
+import autoprefixer from 'autoprefixer';
+import csso from 'postcss-csso';
+import terser from 'gulp-terser';
+import sharp from 'gulp-sharp-responsive';
+import svgo from 'gulp-svgmin';
+import { stacksvg } from "gulp-stacksvg";
+import { deleteAsync } from 'del';
+import browser from 'browser-sync';
+import bemlinter from 'gulp-html-bemlinter';
+
+const sass = gulpSass(dartSass);
+let isDevelopment = true;
+
+export function processMarkup () {
+  return gulp.src('source/*.html')
+    .pipe(gulp.dest('build'));
+}
+
+export function lintBem () {
+  return gulp.src('source/*.html')
+    .pipe(bemlinter());
+}
+
+export function processStyles () {
+  return gulp.src('source/sass/*.scss', { sourcemaps: isDevelopment })
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+      postUrl({ assetsPath: '../' }),
+      autoprefixer(),
+      csso()
+    ]))
+    .pipe(gulp.dest('build/css', { sourcemaps: isDevelopment }))
+    .pipe(browser.stream());
+}
+
+export function processScripts () {
+  return gulp.src('source/js/**/*.js')
+    .pipe(terser())
+    .pipe(gulp.dest('build/js'))
+    .pipe(browser.stream());
+}
+
+export function optimizeImages () {
+  return gulp.src('source/img/**/*.{png,jpg}')
+    .pipe(sharp(isDevelopment ? {
+      includeOriginalFile: true,
+      formats: [
+        { format: 'webp' }
+      ]
+    } : {
+      formats: [
+        {
+          jpegOptions: {
+            progressive: true,
+            mozjpeg: true
+          },
+          pngOptions: {
+            palette: true
+          }
+        },
+        {
+          format: 'webp',
+          webpOptions: {
+            effort: 6
+          }
+        }
+      ]
+    }))
+    .pipe(gulp.dest('build/img'));
+}
+
+export function optimizeVector () {
+  return gulp.src(['source/img/**/*.svg', '!source/img/icons/**/*.svg'])
+    .pipe(svgo())
+    .pipe(gulp.dest('build/img'));
+}
+
+export function createStack () {
+  return gulp.src('source/img/icons/**/*.svg')
+    .pipe(svgo())
+    .pipe(stacksvg())
+    .pipe(gulp.dest('build/img/icons'));
+}
+
+export function copyAssets () {
+  return gulp.src([
+    'source/fonts/**/*.{woff2,woff}',
+    'source/*.ico',
+    'source/*.webmanifest',
+  ], {
+    base: 'source'
+  })
+    .pipe(gulp.dest('build'));
+}
+
+export function startServer (done) {
+  browser.init({
+    server: {
+      baseDir: 'build'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+
+function reloadServer (done) {
+  browser.reload();
+  done();
+}
+
+function watchFiles () {
+  gulp.watch('source/sass/**/*.scss', gulp.series(processStyles));
+  gulp.watch('source/js/script.js', gulp.series(processScripts));
+  gulp.watch('source/*.html', gulp.series(processMarkup, reloadServer));
+}
+
+function compileProject (done) {
+  gulp.parallel(
+    processMarkup,
+    processStyles,
+    processScripts,
+    optimizeVector,
+    createStack,
+    copyAssets,
+    optimizeImages
+  )(done);
+}
+
+function deleteBuild () {
+  return deleteAsync('build');
+}
+
+export function buildProd (done) {
+  isDevelopment = false;
+  gulp.series(
+    deleteBuild,
+    compileProject
+  )(done);
+}
+
+export function runDev (done) {
+  gulp.series(
+    deleteBuild,
+    compileProject,
+    startServer,
+    watchFiles
+  )(done);
+}
---
+++ sass/sass/source/img/README.md
@@ -0,0 +1,30 @@
+Папка для изображений.
+
+Все SVG-иконки предназначенных для спрайта кладите в `img/icons/`:
+```
+img/
+  icons/
+    vk.svg
+```
+
+Остальную векторную и растровую графику кладите непосредственно в папку `img/`:
+```
+img/
+  bg.jpg
+  hero.png
+  burger.svg
+```
+
+Графику можно группировать папками в `img/`:
+```
+img/
+  catalog/
+    product-1.jpg
+    product-2.jpg
+  form/
+    check.jpg
+    uncheck.jpg
+  bg.jpg
+  hero.png
+  burger.svg
+```
