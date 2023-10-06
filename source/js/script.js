d0d55315fd4cf12f0ba429ab5eab8b799d8e2cf8
DizBars <DizBars@yandex.ru>
2023-10-06 21:01:09 +0300

    Second day

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,12 @@
-/* в этот файл добавляет скрипты*/
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
