a7bc6e2a81661303a4dbfdc30414a7c8ed6aa6d3
DizBars <DizBars@yandex.ru>
2023-10-08 11:07:00 +0300

    Slider

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,5 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
@@ -10,3 +10,74 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+const sliderImages = document.querySelectorAll('.slider__item'),
+    sliderLine = document.querySelector('.slider__list'),
+    sliderBullet = document.querySelectorAll('.slider__button'),
+    sliderBulletSpan = document.querySelectorAll('.slider__button-bullet'),
+    sliderBtnNext = document.querySelector('.slider__arrow-right'),
+    sliderBtnPrev = document.querySelector('.slider__arrow-left');
+
+    let sliderCount = 0,
+    sliderWidth;
+
+    sliderBtnNext.addEventListener('click', nextSlide);
+    sliderBtnPrev.addEventListener('click', prevSlide);
+
+// Адаптивность слайдера
+window.addEventListener('resize', showSlide);
+// Кнопки листания слайдов вперед и назад
+// Функции ==================
+// Задает нужную ширину картинки и sliderLine
+function showSlide() {
+    sliderWidth = document.querySelector('.slider').offsetWidth;
+    sliderLine.style.width = sliderWidth * sliderImages.length + 'px';
+    sliderImages.forEach(item => item.style.width = sliderWidth + 'px');
+
+    rollSlider();
+}
+showSlide();
+
+// Перелистывает слайд вперед
+function nextSlide() {
+    sliderBtnPrev.disabled = false;
+    sliderCount++;
+    if(sliderCount >= sliderImages.length-1) sliderBtnNext.disabled = true;
+
+    rollSlider();
+    thisSlide(sliderCount);
+    }
+
+
+// Перелистывает слайд назад
+function prevSlide() {
+    sliderBtnNext.disabled = false;
+    sliderCount--;
+    if(sliderCount <= 0) sliderBtnPrev.disabled = true;
+
+    rollSlider();
+    thisSlide(sliderCount);
+}
+
+
+// Задает шаг перемещения слайдов
+function rollSlider() {
+    sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+}
+
+    function thisSlide(index) {
+      sliderBullet.forEach(item => item.classList.remove('slider__button--active'));
+      sliderBulletSpan.forEach(item => item.classList.remove('slider__button-bullet--active'));
+      sliderBullet[index].classList.add('slider__button--active');
+      sliderBulletSpan[index].classList.add('slider__button-bullet--active');
+  }
+
+
+
+  sliderBullet.forEach((dot, index) => {
+      dot.addEventListener('click', () => {
+          sliderCount = index;
+          rollSlider();
+          thisSlide(sliderCount);
+      })
+  })
