de2c5002fdc3fc6d62204405cef494e1859e7b77
Artur Pletnev <art-pletnev@mail.ru>
2024-01-12 15:58:13 +0300

    Стиллизация мобил

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
-/* в этот файл добавляет скрипты*/
+const toggleButtonElement = document.querySelector('.main-header__button');
+const menuElement = document.querySelector('.main-header__nav');
+const nojsElement = document.querySelector('.no-js');
+
+nojsElement.classList.remove('no-js');
+
+toggleButtonElement.addEventListener('click', function(){
+  toggleButtonElement.classList.toggle('main-header__toggle-closed');
+  menuElement.classList.toggle('main-nav__opened')
+})
