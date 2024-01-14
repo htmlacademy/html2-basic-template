55bfabda82d5d87f90b7197ea4f7ffb8e43a3f9b
Artur Pletnev <art-pletnev@mail.ru>
2024-01-14 19:45:20 +0300

    Формы планшет

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -4,7 +4,7 @@ const nojsElement = document.querySelector('.no-js');
 
 nojsElement.classList.remove('no-js');
 
-toggleButtonElement.addEventListener('click', function(){
+toggleButtonElement.addEventListener('click', function () {
   toggleButtonElement.classList.toggle('main-header__toggle-closed');
   menuElement.classList.toggle('main-nav__opened')
 })
